acc369eab7ec26a8c59abb7e41c4b889faa23ce6
Gurman Bhatia <gurmanbh@users.noreply.github.com>
2021-07-20 23:34:32 +0530

    0 padding in filenames.

--- 2-mueller/script.sh
+++ 2-mueller/script.sh
@@ -8,11 +8,11 @@ curl -L -o report.pdf https://www.justice.gov/storage/report.pdf
 # Now we have the report, so on to step 2
 
 # Turn all pages into pngs
-magick convert report.pdf -resize 25% report.png
+magick convert report.pdf -resize 25% report-%03d.png
 
 # Turn it to a montage
 magick montage -tile 15x0 -geometry +0+0 report*.png grid-withgeo.png
 
 # Or a gif! Maybe the pages should be bigger for that?
-convert -density 72 report.pdf -resize 50% -background white -alpha remove -alpha off report-bigger.png
+convert -density 72 report.pdf -resize 50% -background white -alpha remove -alpha off report-bigger-%03d.png
 convert -delay 20 report-bigger*.png -loop 0  animate.gif
