6eea6ed78cdecf415e1dc986396819f861b4cd3c
Gurman Bhatia <gurmanbh@gmail.com>
2023-07-05 16:22:46 +0530

    mueller update

--- 2-mueller/script.sh
+++ 2-mueller/script.sh
@@ -13,17 +13,20 @@ curl -o report.pdf https://www.justice.gov/storage/report.pdf
 # We use a flag to solve that problem. -L makes sure curl picks the redirected file.
 curl -L -o report.pdf https://www.justice.gov/storage/report.pdf
 # if you get the error "curl: (35) schannel: next InitializeSecurityContext failed: Unknown error (0x80092012) - The revocation function was unable to check revocation for the certificate."
-# use the ss;l-no-revoke flag like this >>>> curl --ssl-no-revoke  -L -o report.pdf https://www.justice.gov/storage/report.pdf
+# use the ssl-no-revoke flag like this >>>> curl --ssl-no-revoke  -L -o report.pdf https://www.justice.gov/storage/report.pdf
+# the ssl-no-revoke flag means that curl will not check the certificate of the website. Use it with caution, as from unknown websites, you might get a malicious file.
 
 # Now we have the report, so on to step 2
 
-# Turn all pages into pngs
-magick convert report.pdf -resize 25% output-%03d.jpg
+# Turn all pages into pngs. Because of all the pages, it might take a while.
+# we resize the images to 25% of the original size. And %03d means 3 digit padding for the numbers.
+magick convert report.pdf -resize 25% report-%03d.png
 
 # Turn it to a montage
 magick montage -tile 15x0 -geometry +0+0 report*.png grid-withgeo.png
 
 # Or a gif! Maybe the pages should be bigger for that?
+# density 72 means 72 dpi. dpi means dots per inch. the computer and screen equivalent of that is pixels per inch.
 # Make bigger images
 magick convert -density 72 report.pdf -resize 50% -background white -alpha remove -alpha off report-bigger-%03d.png
 # GIF it!
