90077d8d5b29afe96044447d3a64c5d3c93195e7
Gurman Bhatia <gurmanbh@gmail.com>
2023-07-05 15:50:09 +0530

    more videos

--- 1-video-to-grid/script.sh
+++ 1-video-to-grid/script.sh
@@ -8,11 +8,11 @@
 
 # Convert our video to a bunch of images. 
 # -i is input. it precedes our filename.
+# -filter:v scale=720:-1 means I want to scale the video to 720 pixels wide and keep the aspect ratio.
 # -r flag defines the framerate. 0.5 frame rate means 1 frame every 2 seconds. 
 # frame rate of 1 means 1 frame every second. 15 means 15 frames every second.
 # %04d means it will assign numbers with a 4 digit padding to the images that are generated. 
-ffmpeg -i hibiscus.mp4 -r 0.5 output_%04d.png
-
+ffmpeg -i hoop.mp4 -filter:v scale=720:-1 -r 1 output_%04d.png
 # Now we'll have a bunch of images in our folder. 
 # We want to turn our images to a grid of images. 
 
@@ -21,12 +21,12 @@ ffmpeg -i hibiscus.mp4 -r 0.5 output_%04d.png
 
 # -tile 6x0 means I want 6 columns and whatever number of rows are needed to complete my grid
 # -geometry +0+0 means no spaces between images
-# then I ask it to take ALL pngs using `*.png`. it is a regex
+# then I ask it to take ALL pngs starting with output using `output*.png`. it is a regex
 # And save the whole thing as out.png
-magick montage -tile 6x0 -geometry +0+0 *.png out.png
+magick montage -tile 2x3 -geometry +0+0 output*.png grid.png
 
 # Another way to do this will be using the legacy montage.
 # both are same and work in a similar manner.
 # basically the one below is the older way of using IM
 # and the one above is a newer way. the legacy tool is better documented.
-montage -tile 6x0 -geometry +0+0 -border 10 *.png out.png
+montage -tile 6x0 -geometry +0+0 -border 10 *.png grid.png
